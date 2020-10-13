68d7eaf345e9edb8635962432e1f41693b3fcbfc
15036786474 <sal13523792289>
2020-10-13 19:15:09 +0800

    日志

--- HnTask/src/main/java/com/edu/zut/rwdb/system/controller/DatabaseController.java
+++ HnTask/src/main/java/com/edu/zut/rwdb/system/controller/DatabaseController.java
@@ -1,6 +1,8 @@
 package com.edu.zut.rwdb.system.controller;
 
 import com.edu.zut.rwdb.system.service.IDatabaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,16 @@ import java.util.Map;
 public class DatabaseController {
     @Autowired
     private IDatabaseService databaseService;
+    private final Logger logger = LoggerFactory.getLogger(DatabaseController.class);
 
     @RequestMapping("/test")
     @ResponseBody
     public List<Map> test(){
-        return databaseService.test();
+        List<Map> list = databaseService.test();
+        logger.debug("debug");
+        logger.error("error");
+        logger.warn("warn");
+        logger.info("info");
+        return list;
     }
 }
