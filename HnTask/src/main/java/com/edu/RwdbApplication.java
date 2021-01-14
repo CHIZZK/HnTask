aa4bbe33a97d520e7c71436bff98d7c24a6fe3d1
15036786474 <sal13523792289>
2021-01-14 19:45:26 +0800

    启动项

--- HnTask/src/main/java/com/edu/RwdbApplication.java
+++ HnTask/src/main/java/com/edu/RwdbApplication.java
@@ -4,10 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan(value = "com.edu.zut.rwdb.system.mapper")
-public class RwdbApplication {
+public class RwdbApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RwdbApplication.class, args);
